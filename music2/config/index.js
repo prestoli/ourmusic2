f8f83b9868db28c5dc6038e564a6129a7633d507
123 <prestoli@163.com>
2017-05-03 11:28:19 +0800

    调试

--- music2/config/index.js
+++ music2/config/index.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 3001,
+    port: 3003,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
